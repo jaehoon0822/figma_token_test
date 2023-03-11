2380af603f6de41bee0eac14e8e5e0d3dccbee4a
Jhoon <jaehoon0822@gmail.com>
2023-03-11 12:56:42 +0000

    transform. create Theme Files

---
+++ theme/js/esm/dark.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 11 Mar 2023 12:56:41 GMT
+ */
+
+export const Primary = "#ffffff";
+export const Secondary = "#000000";
+export const Tertiary = "#2b2b2b";
+export const ContentsContentPrimary = "#ffffff";
+export const ContentsContentSecondary = "#a6a6a6";
+export const ContentsContentTertiary = "#898989";
+export const BorderBorderTransparent = "#8989894d";
+export const BorderBorderOpaque = "#898989";
